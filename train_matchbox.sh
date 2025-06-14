b2dbeaa357ae15204f0ce1f2d7c228f6ba4a538d
sahni <sahni@ningu.net>
2025-06-14 09:47:30 +0000

    parameter parsing for train script

--- train_matchbox.sh
+++ train_matchbox.sh
@@ -3,13 +3,56 @@
 
 # rm -r outputs/train/matchbox/
 
+set -eu
 
-python lerobot/scripts/train.py \
-  --dataset.repo_id=hubnemo/so101_matchbox \
-  --policy.type=smolvla \
-  --output_dir=outputs/train/matchbox \
-  --job_name=matchbox \
-  --policy.device=cuda \
-  --steps=200 \
-  --wandb.enable=false \
+do_resume=false
+num_steps=200
 
+while [ "$#" -gt 0 ]; do
+	case $1 in
+		resume)
+			do_resume=true
+			;;
+		--steps)
+			shift 1
+			num_steps=$1
+			;;
+		*)
+			echo "Usage: $0 [--steps=n] [resume]"
+			exit 1
+			;;
+	esac
+	shift 1
+done
+
+num_warmup_steps="$(echo "${num_steps} * 0.05" | bc -q)"
+num_decay_steps="$num_steps"
+
+if $do_resume; then
+	echo "Resuming for $num_steps steps"
+	python lerobot/scripts/train.py \
+	  --dataset.repo_id=hubnemo/so101_matchbox \
+	  --policy.type=smolvla \
+	  --output_dir=outputs/train/matchbox \
+	  --job_name=matchbox \
+	  --policy.device=cuda \
+	  --policy.warmup_steps=$num_warmup_steps \
+	  --policy.decay_steps=$num_decay_steps \
+	  --steps=$num_steps \
+	  --wandb.enable=false \
+	  --resume=true \
+	  --config_path=outputs/train/matchbox/checkpoints/last/pretrained_model/train_config.json
+else
+	echo Training from scratch
+	python lerobot/scripts/train.py \
+	  --dataset.repo_id=hubnemo/so101_matchbox \
+	  --policy.type=smolvla \
+	  --output_dir=outputs/train/matchbox \
+	  --job_name=matchbox \
+	  --policy.device=cuda \
+	  --policy.warmup_steps=$num_warmup_steps \
+	  --policy.decay_steps=$num_decay_steps \
+	  --steps=$num_steps \
+	  --wandb.enable=false \
+	  --resume=true 
+fi
