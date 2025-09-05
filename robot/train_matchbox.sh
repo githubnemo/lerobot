8b0119a5634e9e5501387bc3a50d9ab8631700f4
NotNANtoN <antonwiehe@gmail.com>
2025-09-05 18:57:33 +0200

    train utils

--- robot/train_matchbox.sh
+++ robot/train_matchbox.sh
@@ -7,8 +7,8 @@ set -eu
 
 do_resume=false
 num_steps=1000
-use_peft=true
-wandb=false
+use_peft=false
+wandb=true
 suffix=""
 
 while [ "$#" -gt 0 ]; do
@@ -44,36 +44,47 @@ num_decay_steps="$num_steps"
 echo "do resume: $do_resume"
 echo "num_warmup_steps: $num_warmup_steps"
 
+MODEL_TYPE="smolvla"
+
 # Clear cache for updated dataset (uncomment if dataset was updated)
 # rm -r ~/.cache/huggingface/lerobot/hubnemo/so101_matchbox
 
-MODEL_NAME="so101_matchbox_smolvla_$(date +%Y%m%d_%H%M)"
+MODEL_NAME="so101_matchbox_${MODEL_TYPE}_$(date +%Y%m%d_%H%M)"
 echo "MODEL_NAME: $MODEL_NAME"
+
+
+REPO_ID="hubnemo/so101_matchbox_reward_dataset_fpv"
 
 # Common arguments for both resume and fresh training
 common_args=(
-  --dataset.repo_id=hubnemo/so101_matchbox_reward
-  --policy.type=smolvla
+  --dataset.repo_id=$REPO_ID
+  --policy.type=$MODEL_TYPE
   --output_dir=outputs/train/matchbox_${MODEL_NAME}
   --job_name=matchbox_${MODEL_NAME}
   --policy.device=cuda
-  --policy.scheduler_warmup_steps="$num_warmup_steps"
-  --policy.scheduler_decay_steps="$num_decay_steps"
   --steps="$num_steps"
   --wandb.enable=$wandb
   --dataset.image_transforms.enable=true
-  --policy.optimizer_lr=1e-4
-  --batch_size=32
+  --policy.optimizer_lr=5e-4
+  --batch_size=8
   --policy.push_to_hub=False
   --log_freq=50
   # Validation settings for supervised learning
   --use_validation=true
-  --val_split=0.2
-  --val_freq=50
-  --val_batch_size=32
+  --val_split=0.02
+  --val_freq=100
+  --val_batch_size=16
   --use_peft=$use_peft
 )
 run_name="matchbox${suffix}"
+
+
+if [ "$MODEL_TYPE" = "smolvla" ]; then
+  common_args+=(
+    --policy.scheduler_warmup_steps="$num_warmup_steps"
+    --policy.scheduler_decay_steps="$num_decay_steps"
+  )
+fi
 
 if $do_resume; then
   echo "Resuming for $num_steps steps"
